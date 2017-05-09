4acb3d350ef41c4cbfbbebd9b2008aaabca8246d
lbluque <lbluque@berkeley.edu>
2017-05-09 09:44:24 -0700

    ...

--- timer.sh
+++ timer.sh
@@ -6,9 +6,9 @@
 #COMMAND="$RUN_PATH/groupF.exe -i $RUN_PATH/input.txt"
 RUN_PATH=./eigen/Code/exec
 COMMAND="$RUN_PATH/parallel.exe -i $RUN_PATH/input.txt"
-OUTFILE=eigen_time.txt
+OUTFILE=new_matrix_times.txt
 
-for size in 10 14 18 22 24 26
+for size in 10 14 18 22 26 30 40 50 65 85
 do
     sed -i "2s/.*/${size}/" $RUN_PATH/input.txt
     echo "Size, ${size}" >> $OUTFILE
